116431431a9d336b15677f7a6348b2c3b40bc6f9
Andrey <vasetskiy777@mail.ru>
2022-03-31 13:57:54 +0700

    New

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import CommandsForFindObstacle
 import numpy as np
 
-# grid = np.load('./result.npy')
+grid = np.load('./result.npy')
 
-grid = [
-    [0, 1, 0, 1, 3, 1, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2],
-    [0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0],
-    [0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 2],
-    [2, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 1, 0],
-]
-path = [(4, 0), (4, 1), (3, 1), (2, 1), (2, 2), (2, 3), (2, 4), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8), (0, 8),
-        (0, 9)]
+# grid = [
+#     [0, 1, 0, 1, 3, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+#     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+#     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2],
+#     [0, 2, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+#     [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 2, 1, 0],
+#     [0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+#     [0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 1, 0],
+# ]
+
 commands = CommandsForFindObstacle.CommandsForFindObstacle(grid)
-for i in range(len(commands.goals)):
-    commands.find_path()
-    commands.prepare_path(path)
-    # commands.print_path()
+commands.find_path()
+# commands.print_path()
+print(commands.path)
+
+commands.find_path()
+# commands.print_path()
+print(commands.path)
+
+commands.find_path()
+# commands.print_path()
+print(commands.path)
+# commands.print_path()
